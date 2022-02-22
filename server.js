fb734a336f9b35e1375f14c9d9c27db619a60383
john.a.clark3 <john.a.clark3@gmail.com>
2022-02-21 19:00:59 -0700

    11.1.6
    installed Heroku and heroku cli
    
    git add -A
    git commit -m "Add Heroku"
    git push heroku feature/MVP:main

--- server.js
+++ server.js
@@ -26,6 +26,7 @@ function filterByQuery(query, animalsArray) {
       // array will then contain only the entries that contain the trait,
       // so at the end we'll have an array of animals that have every one
       // of the traits when the .forEach() loop is finished.
+
       filteredResults = filteredResults.filter(
         (animal) => animal.personalityTraits.indexOf(trait) !== -1
       );
